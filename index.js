0d237aa838e8dd758f2a4f52e5e6204ada431fa0
Victoria Barr <vb329@georgetown.edu>
2014-10-28 16:40:28 +0000

    Connects to sockets

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.use('/public', express.static(__dirname + '/public'));
 
 
 app.get('/', function(request, response){
 	response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('Some doofus connected');
 });
 
 http.listen(3000, function(){
